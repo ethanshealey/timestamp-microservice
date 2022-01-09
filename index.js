caadb647025aeda22006d76d32b1bd29ee6cf4e2
Ethan Shealey <ethan.shealey@gmail.com>
2022-01-08 22:44:58 -0500

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({optionSuccessStatus: 200}))
 app.get('/api/:date?', (req, res) => {
   const date = req.params.date ? new Date(isNaN(req.params.date) ? req.params.date : req.params.date * 1000) : new Date()
   if('Invalid Date' == date) return res.json({ 'error': 'Invalid Date' })
-  return res.json({ 'unix': Math.floor(date.getTime() / 1000), 'utc': date.toGMTString() })
+  return res.json({ 'unix': Math.floor(date.getTime()), 'utc': date.toGMTString() })
 })
 
 app.listen(process.env.PORT || 3000, () => {
